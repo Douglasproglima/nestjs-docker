df5bb1e0bf510b3ad9ec84c8f7f3108c35493a66
Douglasproglima <douglasproglima@gmail.com>
2021-07-07 21:24:09 -0300

    New files docker and docker-compose

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('prefixo')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('test')
+  @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
   @Get('my-route')
   newMethod() {
-    return 'Nova Rota';
+    return 'Nova Rota - Docker';
   }
 }
