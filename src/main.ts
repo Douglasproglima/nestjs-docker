418074e1f1c8301048b4a81ede77ffcc3b8f5ced
Douglasproglima <douglasproglima@gmail.com>
2021-07-08 23:12:09 -0300

    Add global  Pipes and refactore prodController

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common/pipes';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EntityNotFoundExceptionFilter } from './exception-filters/entity-not-found.exception-filter';
@@ -6,6 +7,7 @@ async function bootstrap() {
   //Base do Nestjs: Ele usa o Express, mas pode ser usado o Fastify
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new EntityNotFoundExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422 }));
   await app.listen(3000);
 }
 bootstrap();
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -20,10 +20,7 @@ export class ProductsController {
 
   @HttpCode(201)
   @Post()
-  create(
-    @Body(new ValidationPipe({ errorHttpStatusCode: 422 }))
-    createProductDto: CreateProductDto,
-  ) {
+  create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
@@ -41,11 +38,7 @@ export class ProductsController {
 
   //@Patch(':id'): PUT -> Atualização Completa e o Patch -> Atualização Parcial
   @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body(new ValidationPipe({ errorHttpStatusCode: 422 }))
-    updateProductDto: UpdateProductDto,
-  ) {
+  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(+id, updateProductDto);
   }
 
